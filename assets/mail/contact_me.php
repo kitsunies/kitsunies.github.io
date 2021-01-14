c878d2abc6dc053322a4eabaecdf0bb489876b98
Kitsun <kitsunies@gmail.com>
2021-01-15 07:33:20 +1100

    soot

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,4 +1,5 @@
 <?php
+echo("bruh")
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
@@ -8,7 +9,7 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "kitsunies@gmail.com";
+$to = "tommy@precisionwireless.com";
 $subject = "Website Contact Form: $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
 $header = "From: noreply@gmail.com\n";
